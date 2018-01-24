5821e90697b3d54ceb03c93aee28fdd9db526a52
= <=>
2018-01-24 18:33:09 +0500

    Echo 1

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
 
-$class = new stdClass();
-$class2 = new stdClass();
-
-$class->name = "Anfal";
-
-$class2->name = "Hello";
-$class2->message = "World";
-
-foreach ($class2 as $key => $value) {
-
-    $class->$key = $value;
-
-}
-
-var_dump($class);
+echo 1;
